cec7e7130f3d34c43a272b817f92cd6029f95b3f
lseparatio <consultantaanglia@gmail.com>
2023-01-13 09:37:41 +0000

    Finished navbar and some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import Button from 'react-bootstrap/Button';
+//import logo from './logo.svg';
+//import Button from 'react-bootstrap/Button';
 import NavBar from './components/NavBar';
-import './App.css';
+import styles from './App.module.css';
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route exact path="/" element={<h1>Home page</h1>} />
+          <Route exact path="/about" element={<h1>About</h1>} />
+          <Route exact path="/projects" element={<h1>Projects</h1>} />
+          <Route exact path="/blog" element={<h1>Blog</h1>} />
+          <Route exact path="/contact" element={<h1>Contact</h1>} />
+          <Route exact path="/signin" element={<h1>Sign in</h1>} />
+          <Route exact path="/signup" element={<h1>Sign up</h1>} />
+          <Route element={() => <p>Page not found!</p>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,26 +2,33 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import styles from '../styles/NavBar.module.css'
+import { NavLink } from "react-router-dom";
+
+
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed='top'>
-    <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">About</Nav.Link>
-          <Nav.Link href="#link">Projects</Nav.Link>
-          <Nav.Link href="#link">Blog</Nav.Link>
-          <Nav.Link href="#link">Contact</Nav.Link>
+    <Navbar className={styles.NavBar} expand="md" fixed='top'>
+      <Container>
+        <NavLink exact="true" className="nav-link" to="/">
+          <Navbar.Brand>Ionut Zapototchi</Navbar.Brand>
+        </NavLink>
 
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto text-center">
+            <NavLink exact="true" className="nav-link" to="/">Home</NavLink>
+            <NavLink exact="true" className="nav-link" to="/about">About</NavLink>
+            <NavLink exact="true" className="nav-link" to="/projects">Projects</NavLink>
+            <NavLink exact="true" className="nav-link" to="/blog">Blog</NavLink>
+            <NavLink exact="true" className="nav-link" to="/contact">Contact</NavLink>
+            <NavLink exact="true" className="nav-link" to="/signin">Sign In</NavLink>
+            <NavLink exact="true" className="nav-link" to="/signup">Sign Up</NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
