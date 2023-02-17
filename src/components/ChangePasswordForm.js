a3bea8ada67a04026822fb2b6d43dfcfcf3ee0fd
lseparatio <consultantaanglia@gmail.com>
2023-02-17 14:03:00 +0000

    Added change password form

--- src/components/ChangePasswordForm.js
+++ src/components/ChangePasswordForm.js
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Col, Container, Row, Form, Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Alert from 'react-bootstrap/Alert'
+import styles from "../styles/ChangePasswordForm.module.css"
 
-const ChangePasswordForm = () => {
+
+
+const SignUpForm = () => {
+  const [changePasswordData, setChangePasswordData] = useState({
+    old_password: "",
+    new_password1: "",
+    new_password2: "",
+  });
+  const { old_password, new_password1, new_password2, } = changePasswordData;
+
+  const [errors, setErrors] = useState({});
+  const [revealed, setRevealed] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (event) => {
+    setChangePasswordData({
+      ...changePasswordData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleReveal = () => {
+    setRevealed(!revealed)
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post("/dj-rest-auth/password/change/", changePasswordData);
+      //navigate("/signin");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
   return (
-    <div>ChangePasswordForm</div>
-  )
-}
+    <Container fluid>
+      <Row>
+        <Col md={6} className="d-flex align-items-center justify-content-center">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="old_password">
+              <Form.Label>Old Password</Form.Label>
+              <Form.Control type={(revealed) ? "text" : "password"} placeholder="Old Password" name='old_password' value={old_password} onChange={handleChange} className={styles.Input} />
+            </Form.Group>
+            {errors.old_password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-export default ChangePasswordForm
+
+            <Form.Group className="mb-3" controlId="new_password1">
+              <Form.Label>New Password</Form.Label>
+              <Form.Control type={(revealed) ? "text" : "password"} placeholder="New Password" name='new_password1' value={new_password1} onChange={handleChange} className={styles.Input} />
+            </Form.Group>
+            {errors.new_password1?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group className="mb-3" controlId="new_password2">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type={(revealed) ? "text" : "password"} placeholder="Confirm Password" name='new_password2' value={new_password2} onChange={handleChange} className={styles.Input} />
+            </Form.Group>
+            {errors.new_password2?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group className='mb-3' controlId="revealPassword">
+              <Form.Check type="checkbox" label="Show Passwords" onClick={handleReveal} />
+            </Form.Group>
+            <div className="d-grid gap-2">
+              <Button variant="secondary" type="submit">
+                Change Password
+              </Button>
+            </div>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert key={idx} variant="warning" className="mt-3">
+                {message}
+              </Alert>
+            ))}
+          </Form>
+        </Col>
+      </Row >
+    </Container >
+  );
+}
+export default SignUpForm;
