a8466d7352bd236ac22fe9e376bfc5850afdfc48
lseparatio <consultantaanglia@gmail.com>
2023-03-12 09:23:08 +0000

    Swithed to npm package from fontawesome

--- src/components/ProfileDropdown.js
+++ src/components/ProfileDropdown.js
@@ -1,60 +1,70 @@
-import React from 'react'
+import React from "react";
 import axios from "axios";
-import { useSetCurrentUser, } from '../contexts/CurrentUserContext';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import { useCurrentUser, } from '../contexts/CurrentUserContext';
-import { Link, useNavigate } from 'react-router-dom';
-import { Flip, toast } from 'react-toastify';
-
+import { useSetCurrentUser } from "../contexts/CurrentUserContext";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { Link, useNavigate } from "react-router-dom";
+import { Flip, toast } from "react-toastify";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
 
 function ProfileDropdown() {
-    const currentUser = useCurrentUser();
-    const setCurrentUser = useSetCurrentUser();
-    const user = setCurrentUser[0]
-    const navigate = useNavigate();
+  const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+  const user = setCurrentUser[0];
+  const navigate = useNavigate();
 
-    const handleSignOut = async () => {
-        try {
-            await axios.post("dj-rest-auth/logout/");
-            user(null);
-            navigate("/signin");
-            toast.warn('Your was sign out successfully!', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                transition: Flip,
-                progress: undefined,
-                theme: "light",
-            });
-        } catch (err) {
-            console.log(err);
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      user(null);
+      navigate("/signin");
+      toast.warn("Your was sign out successfully!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Flip,
+        progress: undefined,
+        theme: "light",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-        }
-    };
+  const navDropdownTitle = (
+    <FontAwesomeIcon icon={faGear} size="2xl" fixedWidth transform="up-2" />
+  );
 
-    const navDropdownTitle = (<i className="fa-solid fa-gear fa-2xl"></i>)
-
-    return (
-        <NavDropdown title={navDropdownTitle} id="settings-dropdown" align="end">
-            <NavDropdown.Item as={Link} className="text-center" to={`/profile/${currentUser[0]?.username}`}>
-                Profile
-            </NavDropdown.Item>
-            <NavDropdown.Item className="text-center" href="#action/3.2">
-                Projects
-            </NavDropdown.Item>
-            <NavDropdown.Item className="text-center" href="#action/3.3">
-                Orders
-            </NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item as={Link} to={'/'} className="text-center" onClick={handleSignOut}>
-                Sign Out
-            </NavDropdown.Item>
-        </NavDropdown>
-    )
+  return (
+    <NavDropdown title={navDropdownTitle} id="settings-dropdown" align="end">
+      <NavDropdown.Item
+        as={Link}
+        className="text-center"
+        to={`/profile/${currentUser[0]?.username}`}
+      >
+        Profile
+      </NavDropdown.Item>
+      <NavDropdown.Item className="text-center" href="#action/3.2">
+        Projects
+      </NavDropdown.Item>
+      <NavDropdown.Item className="text-center" href="#action/3.3">
+        Orders
+      </NavDropdown.Item>
+      <NavDropdown.Divider />
+      <NavDropdown.Item
+        as={Link}
+        to={"/"}
+        className="text-center"
+        onClick={handleSignOut}
+      >
+        Sign Out
+      </NavDropdown.Item>
+    </NavDropdown>
+  );
 }
 
-export default ProfileDropdown
-
+export default ProfileDropdown;
--- src/components/SkillsFeature.js
+++ src/components/SkillsFeature.js
@@ -1,48 +1,101 @@
-import React, { useEffect } from 'react'
-import { Col, Row } from 'react-bootstrap';
+import React, { useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
 import styles from "../styles/SkillsFeature.module.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHtml5,
+  faCss3,
+  faSquareJs,
+  faBootstrap,
+  faSquareFontAwesomeStroke,
+  faReact,
+  faWordpress,
+  faGithub,
+  faGit,
+  faAws,
+  faLinux,
+  faUbuntu,
+  faRedhat,
+  faCentos,
+  faStripe,
+} from "@fortawesome/free-brands-svg-icons";
 
 function SkillsFeature() {
-    useEffect(() => {
-        const root = document.documentElement;
-        const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
-        const marqueeContent = document.querySelector("ul.marquee-content");
-        root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+  useEffect(() => {
+    const root = document.documentElement;
+    const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+      "--marquee-elements-displayed"
+    );
+    const marqueeContent = document.querySelector("ul.marquee-content");
+    root.style.setProperty(
+      "--marquee-elements",
+      marqueeContent.children.length
+    );
 
-        for (let i = 0; i < marqueeElementsDisplayed; i++) {
-            marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
-        }
-    });
+    for (let i = 0; i < marqueeElementsDisplayed; i++) {
+      marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+    }
+  });
 
-    return (
-        <Row className={styles.Row} md={12}>
-            <Col md={2} className={`d-flex align-items-center justify-content-center ${styles.TextCol}`}>
-                <h5>
-                    The technology utilized in my projects:
-                </h5>
-            </Col>
-            <Col md={10} className={`marquee ${styles.Marquee}`}>
-                <ul className={`marquee-content ${styles.MarqueeContent}`}>
-                    <li><i class="fa-brands fa-html5"></i></li>
-                    <li><i class="fa-brands fa-css3-alt"></i></li>
-                    <li><i class="fa-brands fa-square-js"></i></li>
-                    <li><i class="fa-brands fa-bootstrap"></i></li>
-                    <li><i class="fa-brands fa-square-font-awesome-stroke"></i></li>
-                    <li><i class="fab fa-react"></i></li>
-                    <li><i class="fab fa-wordpress"></i></li>
-                    <li><i class="fab fa-github"></i></li>
-                    <li><i class="fa-brands fa-git"></i></li>
-                    <li><i class="fab fa-aws"></i></li>
-                    <li><i class="fa-brands fa-linux"></i></li>
-                    <li><i class="fa-brands fa-ubuntu"></i></li>
-                    <li><i class="fa-brands fa-redhat"></i></li>
-                    <li><i class="fa-brands fa-centos"></i></li>
-                    <li><i class="fa-brands fa-stripe"></i></li>
-                </ul>
-            </Col>
-        </Row>
-    )
+  return (
+    <Row className={styles.Row} md={12}>
+      <Col
+        md={2}
+        className={`d-flex align-items-center justify-content-center ${styles.TextCol}`}
+      >
+        <p>The technology utilized in my projects:</p>
+      </Col>
+      <Col md={10} className={`marquee ${styles.Marquee}`}>
+        <ul className={`marquee-content ${styles.MarqueeContent}`}>
+          <li>
+            <FontAwesomeIcon icon={faHtml5} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCss3} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSquareJs} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBootstrap} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSquareFontAwesomeStroke} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faReact} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faWordpress} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faGithub} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faGit} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faAws} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faLinux} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faUbuntu} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faRedhat} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCentos} />
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faStripe} />
+          </li>
+        </ul>
+      </Col>
+    </Row>
+  );
 }
 
-export default SkillsFeature
+export default SkillsFeature;
