23e85fc79b81f1718c02c8beca92799e7ed86000
lseparatio <consultantaanglia@gmail.com>
2023-03-18 07:47:12 +0000

    Added relevant text content to what i do component

--- src/components/WhatIDo.js
+++ src/components/WhatIDo.js
@@ -13,56 +13,59 @@ const WhatIDo = () => {
       <Row>
         <Col md={6} sm={12}>
           <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header className={styles.AccordionHeader} >Content To fallow</Accordion.Header>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header className={styles.AccordionHeader}>Planning / User Stories</Accordion.Header>
               <Accordion.Body className={styles.AccordionBody}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+                The first step is to determine the purpose of the website,
+                the target audience, and the goals that the website is meant to achieve.
+                This also involves creating a sitemap and wireframes to define the website's
+                structure and content.
               </Accordion.Body>
             </Accordion.Item>
             <Accordion.Item eventKey="1">
-              <Accordion.Header className={styles.AccordionHeader}>Under Construction</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+              <Accordion.Header className={styles.AccordionHeader}>UX/UI Design</Accordion.Header>
+              <Accordion.Body className={styles.AccordionBody}>
+                Once the planning is done, the next step is to design the website's visual elements,
+                including the layout, color scheme, typography, and images.
+                This involves creating a design mockup to demonstrate how the website will look and feel.
+              </Accordion.Body>
+            </Accordion.Item>
+
+            <Accordion.Item eventKey="2">
+              <Accordion.Header className={styles.AccordionHeader}>Development</Accordion.Header>
+              <Accordion.Body className={styles.AccordionBody}>
+                With the design finalized, the website can move into the development phase.
+                This involves writing the code for the website's front-end and back-end functionality,
+                including HTML, CSS, JavaScript, and server-side languages like Python.
+                I usually use React for Frontend and Django for backend.
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
         </Col>
         <Col md={6} sm={12}>
           <Accordion defaultActiveKey="0" flush>
-            <Accordion.Item eventKey="3">
-              <Accordion.Header className={styles.AccordionHeader}>UX/UI Design</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+            <Accordion.Item eventKey="4">
+              <Accordion.Header className={styles.AccordionHeader}>Content Creation</Accordion.Header>
+              <Accordion.Body className={styles.AccordionBody}>
+                While the development work is being done, the website's content needs to be created.
+                This includes writing text content, creating images and videos, and sourcing other relevant materials.
               </Accordion.Body>
             </Accordion.Item>
-            <Accordion.Item eventKey="1">
-              <Accordion.Header className={styles.AccordionHeader}>Under Construction</Accordion.Header>
-              <Accordion.Body>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                culpa qui officia deserunt mollit anim id est laborum.
+            <Accordion.Item eventKey="5">
+              <Accordion.Header className={styles.AccordionHeader}>Testing and Quality Assurance</Accordion.Header>
+              <Accordion.Body className={styles.AccordionBody}>
+                After the development and content creation are done,
+                the website needs to be tested to ensure that it works properly and is free of errors.
+                This involves testing for usability, accessibility,
+                and compatibility with different devices and browsers.
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="6">
+              <Accordion.Header className={styles.AccordionHeader}>Launch/ Deployment</Accordion.Header>
+              <Accordion.Body className={styles.AccordionBody}>
+                Once the website has been thoroughly tested and refined,
+                it is ready to be launched. This involves making the website live on a server and configuring any necessary
+                settings to ensure that it is accessible to visitors. I am proud to say that i can deploy in any chosen environment: Dedicated Server, VPS, AWS.
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
