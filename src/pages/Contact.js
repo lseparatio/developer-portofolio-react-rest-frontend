6ef40d7ca9806bdde3326b76cda85eb8c08e9305
lseparatio <consultantaanglia@gmail.com>
2023-03-24 07:30:20 +0000

    Added interactive message for successfull contact form submision

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,12 +7,11 @@ import Alert from 'react-bootstrap/Alert'
 import { Col, Container, Row, Image } from 'react-bootstrap';
 import contactImage from "../assets/img/contact.png"
 import styles from "../styles/Contact.module.css";
-import { Flip, toast } from 'react-toastify';
+import contactSuccess from '../assets/img/contact-succes.png'
 
 
 
 const Contact = () => {
-
 
     const [contactData, setContactData] = useState({
         full_name: "",
@@ -24,7 +23,7 @@ const Contact = () => {
     const { full_name, email, sent_message, } = contactData;
     const [errors, setErrors] = useState({});
     const [showAlert, setShowAlert] = useState(true);
-
+    const [sent, setSent] = useState(false);
 
 
     const handleChange = (event) => {
@@ -39,23 +38,67 @@ const Contact = () => {
         event.preventDefault();
         try {
             await axios.post("contact/", contactData);
-
-            toast.success('Your message was successfully!', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                transition: Flip,
-                progress: undefined,
-                theme: "light",
-            });
+            setSent(true);
         } catch (err) {
             setShowAlert(true);
             setErrors(err.response?.data);
         }
     };
+
+
+    const successImage = (
+        <>
+            <Image fluid src={contactSuccess} alt='Two persones filling a form' className={styles.ContactSucessImg}></Image>
+            <p className={styles.SuccessP}>Your message was successfully!</p>
+        </>
+
+    );
+
+
+    const contactForm = (
+        <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-4" controlId="full_name">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control type="text" placeholder="Full Name" name='full_name' value={full_name} onChange={handleChange} className={styles.Input} />
+            </Form.Group>
+            {errors.full_name?.map((message, idx) => (
+                <Alert show={showAlert} variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+            <Form.Group className="mb-4" controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email" name='email' value={email} onChange={handleChange} className={styles.Input} />
+            </Form.Group>
+            {errors.email?.map((message, idx) => (
+                <Alert show={showAlert} variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <Form.Group className="mb-3" controlId="sent_message">
+                <Form.Label>Your Message</Form.Label>
+                <Form.Control as="textarea" rows={4} placeholder="Your Message" name='sent_message' value={sent_message} onChange={handleChange} className={styles.Input} />
+            </Form.Group>
+            {errors.sent_message?.map((message, idx) => (
+                <Alert show={showAlert} variant="warning" key={idx}>
+                    {message}
+                </Alert>
+            ))}
+
+            <div className="d-grid gap-2">
+                <Button variant="secondary" size="lg" type="submit">
+                    Send Message
+                </Button>
+            </div>
+            {errors.non_field_errors?.map((message, idx) => (
+                <Alert show={showAlert} key={idx} variant="warning" className="mt-3">
+                    {message}
+                </Alert>
+            ))}
+        </Form>
+    )
+
 
     return (
         <Container fluid className={styles.Container}>
@@ -63,51 +106,13 @@ const Contact = () => {
                 <Col md={6} className="d-flex align-items-center justify-content-center">
                     <Image fluid src={contactImage} alt='Two persones filling a form'></Image>
                 </Col>
-                <Col md={6} className="align-items-center justify-content-center">
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group className="mb-4" controlId="full_name">
-                            <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" placeholder="Full Name" name='full_name' value={full_name} onChange={handleChange} className={styles.Input} />
-                        </Form.Group>
-                        {errors.full_name?.map((message, idx) => (
-                            <Alert show={showAlert} variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-                        <Form.Group className="mb-4" controlId="email">
-                            <Form.Label>Email Address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter Email" name='email' value={email} onChange={handleChange} className={styles.Input} />
-                        </Form.Group>
-                        {errors.email?.map((message, idx) => (
-                            <Alert show={showAlert} variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-
-                        <Form.Group className="mb-3" controlId="sent_message">
-                            <Form.Label>Your Message</Form.Label>
-                            <Form.Control as="textarea" rows={4} placeholder="Your Message" name='sent_message' value={sent_message} onChange={handleChange} className={styles.Input} />
-                        </Form.Group>
-                        {errors.sent_message?.map((message, idx) => (
-                            <Alert show={showAlert} variant="warning" key={idx}>
-                                {message}
-                            </Alert>
-                        ))}
-
-                        <div className="d-grid gap-2">
-                            <Button variant="secondary" size="lg" type="submit">
-                                Send Message
-                            </Button>
-                        </div>
-                        {errors.non_field_errors?.map((message, idx) => (
-                            <Alert show={showAlert} key={idx} variant="warning" className="mt-3">
-                                {message}
-                            </Alert>
-                        ))}
-                    </Form>
+                <Col md={6} id='contactForm' className="align-items-center justify-content-center">
+                    {sent ? successImage : contactForm}
                 </Col>
             </Row>
         </Container>
     );
 }
+
+
 export default Contact;
